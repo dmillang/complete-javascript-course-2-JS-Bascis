98684ae6e65d643eee1f1b6c90740d064a4326b9
David Millan <hello@davidmillan.me>
2019-06-23 11:52:10 +0100

    Variables and Data Type

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+/****************************
+Vairables and Data Types
+*/
+
+var firstName = 'David';
+console.log(firstName);
+
+var lastName = 'Millan';
+var age = 30;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Web Designer';
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+//var if = 0;
