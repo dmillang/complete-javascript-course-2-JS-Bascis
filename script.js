e0cb5e637ba35ac9ce2eab8230fd0c630c84173f
David Millan <hello@davidmillan.me>
2019-06-24 23:59:30 +0100

    Vairable mutation and type coercion lesson

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 Vairables and Data Types
 */
 
+/*
 var firstName = 'David';
 console.log(firstName);
 
@@ -21,3 +22,32 @@ console.log(job);
 var _3years = 3;
 var johnMark = 'John and Mark';
 //var if = 0;
+*/
+
+/****************************
+Vairable mutation and type coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+
+// Type coercion:
+// automatically changing a number data type variable into a string:
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+// Variable mutation:
+// Changing the value of a variable
+
+age = 30;
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
