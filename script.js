2b7cffdd0ced33f0c4d35c3d974477c38c9a0199
David Millan <hello@davidmillan.me>
2019-09-09 22:51:58 +0100

    Complete S2.11 Operator Precedence

--- script.js
+++ script.js
@@ -58,28 +58,63 @@ console.log(firstName + ' ' + lastName);
 Basic operators
 */
 
-var year, ageJohn, ageMark, yearJohn, yearMark;
-year = 2019;
-ageJohn = 28;
-ageMark = 35;
+// var year, ageJohn, ageMark, yearJohn, yearMark;
+// year = 2019;
+// ageJohn = 28;
+// ageMark = 35;
 
-// Math operators
-yearJohn = year - ageJohn;
-yearMark = year - ageMark;
+// // Math operators
+// yearJohn = year - ageJohn;
+// yearMark = year - ageMark;
 
-console.log('John was born in ' + yearJohn + ', while Mark was born in ' + yearMark + '.');
+// console.log('John was born in ' + yearJohn + ', while Mark was born in ' + yearMark + '.');
 
-console.log(year + 2);
-console.log(year * 2);
-console.log(year / 2);
+// console.log(year + 2);
+// console.log(year * 2);
+// console.log(year / 2);
 
-// Logical operators
-var johnOlder = ageJohn > ageMark;
-console.log(johnOlder);
+// // Logical operators
+// var johnOlder = ageJohn > ageMark;
+// console.log(johnOlder);
 
-//typeof operator
-console.log(typeof ageJohn);
-console.log(typeof johnOlder);
-console.log(typeof 'Hi string');
-var x;
-console.log(typeof x);
+// //typeof operator
+// console.log(typeof ageJohn);
+// console.log(typeof johnOlder);
+// console.log(typeof 'Hi string');
+// var x;
+// console.log(typeof x);
+
+/****************************
+Operator precedence
+*/
+
+var now, ageJohn, fullAge;
+
+now = 2019;
+yearJohn = 1989;
+fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; //true
+console.log(isFullAge);
+
+// Grouping (parenthesis)
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var ageAverage = (ageJohn + ageMark) / 2;
+console.log(ageJohn);
+console.log(ageAverage);
+
+// Multiple assignments
+var x,y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x,y);
+
+// More operators
+var z = 5;
+z *= 5; // 25
+console.log('z *= 5 equals ' + z);
+z += 5;
+console.log('z += 5 equals "z = 25 + 5" which equals ' + z); // 30
+z++;
+console.log('++z equals ' + z); // 31
